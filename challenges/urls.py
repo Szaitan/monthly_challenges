7652dd5ca8a12da6648682bb9cf2eedece1104d4
Michał <m.krepiniewicz@gmail.com>
2023-10-14 16:40:02 +0200

    Review 2: Reverse dynamic URL, implementation of simple HTML code into view

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.monthly_challenges_all),
     path("<int:month>", views.monthly_challenge_by_num),
-    path("<str:month>", views.monthly_challenge)
+    path("<str:month>", views.monthly_challenge, name="month-challenge")
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,36 +1,44 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 monthly_challenges = {
-    "january": "January month!",
-    "february": "february month!",
-    "march": "march month!",
-    "april": "april month!",
-    "may": "may month!",
-    "jun": "jun month!",
-    "july": "july month!",
-    "august": "august month!",
-    "september": "september month!",
-    "october": "october month!",
-    "november": "november month!",
-    "december": "december month!",
+    "January": "January month!",
+    "February": "february month!",
+    "March": "march month!",
+    "April": "april month!",
+    "May": "may month!",
+    "Jun": "jun month!",
+    "July": "july month!",
+    "August": "august month!",
+    "September": "september month!",
+    "October": "october month!",
+    "November": "november month!",
+    "December": "december month!",
 }
 
 
 def monthly_challenge_by_num(request, month):
     try:
         redirect_month = list(monthly_challenges.keys())[month-1]
-        print(redirect_month)
     except IndexError:
-        return HttpResponseNotFound(f"There is no data for {month} input")
+        return HttpResponseNotFound(f"<h1>There is no data for {month} input</h1>")
 
-    return HttpResponseRedirect("/challenges/" + redirect_month)
+    redirect_path = reverse("month-challenge", args=[redirect_month]) #Tworzy ścieżkę /challenges/miesiąc
+    return HttpResponseRedirect(redirect_path)
 
 
 def monthly_challenge(request, month):
     try:
-        display = monthly_challenges[month]
+        challenge_text = monthly_challenges[month]
+        response_data = f"<h1>{challenge_text}</h1>"
     except KeyError:
-        return HttpResponse(f"This {month} is not supported by page.")
-    return HttpResponse(display)
+        return HttpResponse(f"<h1>This {month} is not supported by page.</h1>")
+    return HttpResponse(response_data)
+
+
+def monthly_challenges_all(request):
+    # My solution with using
+    response_data = [f"<a href={reverse('month-challenge', args=[key])}><h1>{key}</h1></a>" for key, values in monthly_challenges.items()]
+    return HttpResponse(response_data)
